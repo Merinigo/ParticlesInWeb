b94f3aa312dedccaf440661610dc41354b713bd2
Iñigo Merino <merinigo@gmail.com>
2016-11-28 23:34:34 +0100

    Parece una fuente
    -Mas particulas
    -Gravedad añadida
    -Parece una fuente
    -Cambio de color
    -Funcion crearParticulas añadida

--- particles.js
+++ particles.js
@@ -2,33 +2,31 @@ window.onload = function(){
   
   var canvas = document.createElement("canvas"),
       ctx = canvas.getContext("2d"),
+      refresRate = 30; //Tasa de refresco del canvas (ms)
       particles = {}, //Donde se guarda cada particula
       particleIndex = 0, //Indice de las particulas
-      particleNum = 10; //Cantidad de particulas
+      particleNum = 10; //Cantidad de particulas por refresco
+  
   
   canvas.width = 400;
   canvas.height = 400;
   
   document.body.appendChild(canvas);
       
-  eraseCanvas();
-  
-  var p = new Particle();
+  eraseCanvas();    
 
-  for (var i=0; i < particleNum; i++){
-    new Particle();
-  }
   
   
   setInterval(function(){
     eraseCanvas();
+    createParticles(particleNum);
     
     for(var i in particles){
       particles[i].draw();
     }
     
     p.draw();
-  }, 30);
+  }, refresRate);
   
   //CLASES------------------------
   
@@ -38,8 +36,9 @@ window.onload = function(){
     this.y = canvas.height /2;
     this.vx = Math.random() * 10 - 5;
     this.vy = Math.random() * 10 - 5;
+    this.gravity = 0.3;
     this.life = 0;
-    this.maxLife = 100;
+    this.maxLife = Math.random()*30+10;
     particleIndex++;
     particles[particleIndex] = this; //Se añade la nueva particula
     this.id = particleIndex;         //id nos indica la posicion en la que esta esa particula
@@ -48,16 +47,24 @@ window.onload = function(){
   Particle.prototype.draw = function(){
     this.x += this.vx;
     this.y += this.vy;
+    this.vy += this.gravity;
     this.life++;
-    if(this.life == this.maxLife){
+    if(this.life >= this.maxLife){
       delete particles[this.id];
     }    
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "rgba(255,255,255,0.5)";
     ctx.fillRect(this.x, this.y, 10,10);
   }
       
   
   //FUNCIONES---------------------------
+  
+  //Crea particulas 
+  function createParticles(cantidad){
+    for (var i=0; i < cantidad; i++){
+    new Particle();
+  }
+  }
   
   //Borra el lienzo a negro
   function eraseCanvas(){
