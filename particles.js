07e5ae219634c0a87c73cb4950508d5f9d535a64
Iñigo Merino <merinigo@gmail.com>
2016-11-28 23:20:43 +0100

    Ordenado en clases y funciones
    -Creacion clase particula y metodos añadidos
    -Concepto de vida de particula
    -Mayor cantidad de particulas

--- particles.js
+++ particles.js
@@ -1,7 +1,10 @@
 window.onload = function(){
   
   var canvas = document.createElement("canvas"),
-      ctx = canvas.getContext("2d");
+      ctx = canvas.getContext("2d"),
+      particles = {}, //Donde se guarda cada particula
+      particleIndex = 0, //Indice de las particulas
+      particleNum = 10; //Cantidad de particulas
   
   canvas.width = 400;
   canvas.height = 400;
@@ -10,35 +13,57 @@ window.onload = function(){
       
   eraseCanvas();
   
-  var posX = 20,
-      posY = canvas.height / 2,
-      vx = 5,
-      vy = -10,
-      gravity = 1;
-      
-  
-  setInterval(function(){
-    eraseCanvas();
-    posX += vx;
-    posY += vy;
-    
-    if(posY > 300){
-      vy *= -0.5;
-      vx *= -0.5;
-      posY = 300;
-    }
-    
-    vy += gravity;
-    
-    ctx.fillStyle = "white";
-    ctx.fillRect(posX,posY, 10, 10);
-  }, 30)
-  
-  function eraseCanvas(){
-    ctx.fillStyle = "black";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+  var p = new Particle();
+
+  for (var i=0; i < particleNum; i++){
+    new Particle();
   }
   
   
+  setInterval(function(){
+    eraseCanvas();
+    
+    for(var i in particles){
+      particles[i].draw();
+    }
+    
+    p.draw();
+  }, 30);
+  
+  //CLASES------------------------
+  
+  //Se crea clase particle
+  function Particle(){
+    this.x = canvas.width /2;
+    this.y = canvas.height /2;
+    this.vx = Math.random() * 10 - 5;
+    this.vy = Math.random() * 10 - 5;
+    this.life = 0;
+    this.maxLife = 100;
+    particleIndex++;
+    particles[particleIndex] = this; //Se añade la nueva particula
+    this.id = particleIndex;         //id nos indica la posicion en la que esta esa particula
+  } 
+  //Se añade el metodo draw a clase particle
+  Particle.prototype.draw = function(){
+    this.x += this.vx;
+    this.y += this.vy;
+    this.life++;
+    if(this.life == this.maxLife){
+      delete particles[this.id];
+    }    
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.x, this.y, 10,10);
+  }
+      
+  
+  //FUNCIONES---------------------------
+  
+  //Borra el lienzo a negro
+  function eraseCanvas(){
+      ctx.fillStyle = "black";
+      ctx.fillRect(0,0,canvas.width,canvas.height);
+  }
 }
+
 
